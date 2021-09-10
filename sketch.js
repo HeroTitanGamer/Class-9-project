07c5fa0011f20d8542e45cd8c8fa01f1959a91cb
HeroTitanGamer <89516804+HeroTitanGamer@users.noreply.github.com>
2021-09-10 08:22:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var num
+var anather_num
+var and_anather_num
+function setup (){
+  createCanvas(600,400);
+  num = 0;
+  anather_num = 5;
+  anather_num = 1;
+}
+
+function draw (){
+ 
+  if (keyDown("q")){
+    background("red");
+  }
+  if (keyDown("w")){
+    background("blue");
+    }
+  if (keyDown("e")){
+     background("green");
+     }
+  if (keyDown("r")){
+     background("orange");
+     }  
+  if (keyDown("t")){
+    background("cyan")
+  }
+  if (keyDown("y")){
+    background("white");
+    }  if (keyDown("u")){
+      background("black");
+      }  if (keyDown("i")){
+        background("darkblue");
+        }  if (keyDown("o")){
+          background("pink");
+          }  if (keyDown("p")){
+            background("lime");
+            }  if (keyDown("a")){
+              background("maroon");
+              }  if (keyDown("s")){
+                background("brown");
+                }  if (keyDown("d")){
+                  background("");
+                  }  if (keyDown("f")){
+                    background("darkgreen");
+                    }  if (keyDown("g")){
+                      background("lightblue");
+                      }  if (keyDown("h")){
+                        background("yellow");
+                        }  if (keyDown("j")){
+                          background("lightgreen");
+                          }
+                          
+                          if (keyDown("k")){
+                            background(25);
+                            }
+                            if (keyDown("l")){
+                              background(176);
+                              }
+                              if (keyDown("z")){
+                                background(64);
+                                }  if (keyDown("x")){
+                                  background("196");
+                                  }  if (keyDown("c")){
+                                    background("purple");
+                                    }  if (keyDown("v")){
+                                      background("lime");
+                                      }  if (keyDown("b")){
+                                        background("red");
+                                        }  if (keyDown("n")){
+                                          background("blue");
+                                          }  if (keyDown("m")){
+                                            background("cyan");
+                                              }
+
+
+
+
+
+
+
+
+    }
